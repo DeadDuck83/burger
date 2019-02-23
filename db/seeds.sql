5c9fb47acd20bd69938c05db461e08aa88ecb102
Derek Moore <moore8577@gmail.com>
2019-02-22 20:07:25 -0800

    Connection, schema, Seed set up

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Turbo Turkey", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hunky Monkey", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Sam..ich", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Racoon", true);
